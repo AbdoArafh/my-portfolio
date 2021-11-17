7e9540f0ac221b78cf2df49e67b8c1c967cb3e8c
Abdurrahman <abdoarafh54@gmail.com>
2021-11-17 17:25:50 +0200

    added page routing

--- main.js
+++ main.js
@@ -1,13 +1,26 @@
 const pages = [...document.querySelectorAll(".page")];
 
-pages[0].classList.add("shown");
+let index = 0;
+
+let [key, pageNum] = window.location.search.substr(1).split("=");
+
+if (key === "page" && !isNaN(pageNum) && Number(pageNum) < pages.length && Number(pageNum) >= 0) {
+    index = Number(pageNum);
+}
+
+showPage(index);
 
 document.querySelectorAll(".sidebar-item").forEach(
-    el => el.addEventListener("click", showPage)
+    el => el.addEventListener("click", handleClick)
 );
 
-function showPage(event) {
+function handleClick(event) {
     const id = Number(event.target.id);
+    window.history.pushState({}, null, "?page=" + id.toString());
+    showPage(id);
+}
+
+function showPage(id) {
     for (let page of pages) {
         page.classList.remove("shown");
     }
